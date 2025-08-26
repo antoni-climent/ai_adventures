3b4383920203b2d1f13c6b5b8d671b5ad22e182e
antoni-climent <tonicliment98@gmail.com>
2025-08-26 12:23:08 +0200

    hrm now runs, but loss do not decrease properly

--- hrm.py
+++ hrm.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+torch.autograd.set_detect_anomaly(True)
 
 def get_data_sample():
     n1 = torch.randint(0, 255, ()) # It will use 8 bits
@@ -15,7 +16,9 @@ def get_data_sample():
     n2 = (8 - len(n2))*[0] + n2 
     s = (9 - len(s))*[0] + s # To represent up to 510 we need 9 bits
     
-    return torch.cat((torch.tensor(n1),torch.tensor(n2))), torch.tensor(s)
+    x = torch.cat((torch.tensor(n1, dtype=torch.long),torch.tensor(n2, dtype=torch.long)))
+    y = torch.tensor(s, dtype=torch.float32)
+    return x, y
 
 class HRM(nn.Module):
     
@@ -31,25 +34,17 @@ class HRM(nn.Module):
 
         self.moduleO = nn.Linear(in_features=d_model, out_features=out_size)
 
-    def forward(self, z, x, N=2, T=2):
-        sigm = nn.Sigmoid()
+        self.criterion = nn.BCEWithLogitsLoss()
 
+    def forward(self, z, x, N=2, T=2):
         zH = z[0]
         zL = z[1]
         n_emb = self.num_embed(x)
         pos_emb = self.pos_embed(torch.arange(0,16))
-        print(n_emb.size())
-        print(pos_emb.size())
         final_emb = torch.add(n_emb, pos_emb) # shape: (8,d_model)
         with torch.no_grad():
             for _i in range(N*T - 1):
-                print("final_emb:", final_emb.size())
-                print("zH", zH.size())
-                print("zL", zL.size())
-                # zH = zH.view(1,-1)
-                # zL = zL.view(1,-1)
                 inputL = final_emb + zH + zL
-                print("inputL", inputL.size())
                 zL = self.moduleL(inputL)
                 
                 if (_i + 1)%T == 0:
@@ -63,25 +58,19 @@ class HRM(nn.Module):
         inputH = zH + zL
         zH = self.moduleH(inputH)
         pool = zH.mean(dim=0)
-        print("pool:", pool.size())
-        output_head = self.moduleO(pool)
-
-        print("output_head:", output_head)
-        output = sigm(output_head)
+        output = self.moduleO(pool)
 
         return [zH, zL], output
 
     def train_net(self, z_init, optimizer, epochs, N_supervision):
-        bce= nn.BCELoss()
-        
         for _ in range(epochs):
             x, y_true = get_data_sample()
             z = [z_init, z_init]
             for _ in range(N_supervision):
-                z, y_hat = self.forward(z, x) 
-                loss = bce(y_hat, y_true)
+                z, y_hat = self.forward(z, x, N=20, T=5) 
+                loss = self.criterion(y_hat, y_true)
                 optimizer.zero_grad()
-                loss.bachward()
+                loss.backward()
                 optimizer.step()
                 
                 # Prevent the gradients to influence next round
@@ -92,11 +81,11 @@ class HRM(nn.Module):
 
 
 if __name__ == "__main__":
-    d_model = 512
-    model = HRM(d_model, 4, 8, 9)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
+    d_model = 16
+    model = HRM(d_model=d_model, nhead=2, num_layers=2, out_size=9)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.0000000001)
 
     # Training 
-    z = torch.empty(d_model, dtype=torch.float16)
+    z = torch.empty(d_model, dtype=torch.float32)
     nn.init.trunc_normal_(z, mean=0, std=1.0, a=-2.0, b=2.0)
     model.train_net(z, optimizer, epochs=10000, N_supervision=10)
